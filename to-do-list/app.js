ed2462b25cc55d3d8e38ce7cd62945261c655b82
jiujieti <melody2014ymq@gmail.com>
2017-05-22 22:51:20 +0200

    only static html loaded. To be fixed.

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,10 +1,15 @@
 // load HTTP module
-var http = require("http");
+var http = require('http'),
+    fs = require('fs');
 
 // create HTTP server and listen on port 8000
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8000);
-
-console.log('server running at http://127.0.0.1:8000');
+fs.readFile('./main.html', function (err, html) {
+  if(err) {
+    throw err;
+  }
+  http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(html);
+    response.end();
+  }).listen(8000);
+});
